adce9f9e407a8bdb961b3eb87d542e164d212b2b
Javan <javan_dhein@outlook.com>
2023-02-04 18:40:34 -0300

    feat: Resizer created and edges mod.

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,27 @@ import { zinc } from "tailwindcss/colors";
 import "reactflow/dist/style.css";
 import { Square } from "./components/nodes/Square";
 import { useCallback } from "react";
+import DefaultEdge from "./components/edges/DefaultEdge";
+import * as ToolBar from '@radix-ui/react-toolbar'
 
 const NODE_TYPES = {
   square: Square,
 };
+const EDGE_TYPES = {
+  default: DefaultEdge
+}
+
 const INITIAL_NODES = [
   {
     id: crypto.randomUUID(),
     type: "square",
-    position: { x: 250, y: 250 },
+    position: { x: 200, y: 400 },
     data: {},
   },
   {
     id: crypto.randomUUID(),
     type: "square",
-    position: { x: 850, y: 250 },
+    position: { x: 1000, y: 400 },
     data: {},
   },
 ] satisfies Node[]; // TODO: Add initial nodes
@@ -37,20 +43,41 @@ function App() {
     return setEdges((edges) => addEdge(connection, edges));
   }, []);
 
+  function addSquareNode() {
+    setNodes(nodes => [
+      ...nodes,
+      {
+        id: crypto.randomUUID(),
+        type: "square",
+        position: { x: 750, y: 350 },
+        data: {},
+      },
+    ])
+  }
+
   return (
     <div className="w-screem h-screen">
       <ReactFlow
         nodeTypes={NODE_TYPES}
+        edgeTypes={EDGE_TYPES}
         nodes={nodes}
         connectionMode={ConnectionMode.Loose}
         edges={edges}
         onEdgesChange={onEdgesChange}
         onNodesChange={onNodesChange}
         onConnect={onConnect}
+        defaultEdgeOptions={{
+          type: 'default'
+        }}
       >
         <Background gap={12} size={2} color={zinc[200]} />
         <Controls />
       </ReactFlow>
+      <ToolBar.Root className="fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden">
+        <ToolBar.Button
+          onClick={addSquareNode}
+          className="w-32 h-32 bg-violet-500 mt-6 rounded transition-transform hover:-translate-y-3" />
+      </ToolBar.Root>
     </div>
   );
 }
--- src/components/nodes/Square.tsx
+++ src/components/nodes/Square.tsx
@@ -1,8 +1,20 @@
+import { NodeResizer } from "@reactflow/node-resizer";
 import { NodeProps, Handle, Position } from "reactflow";
+import '@reactflow/node-resizer/dist/style.css';
 
-export function Square(props: NodeProps) {
+export function Square({ selected }: NodeProps) {
   return (
-    <div className="bg-violet-500 rounded w-[200px] h-[200px]">
+    <div className="bg-violet-500 rounded w-full h-full min-w-[200px] min-h-[200px]">
+      <NodeResizer
+        minWidth={200}
+        minHeight={200}
+        isVisible={selected}
+        lineClassName="border-blue-400"
+        handleClassName="h-3 w-3 bg-white border-2 rounded border-blue-400"
+
+      />
+
+
       <Handle
         className="-right-5 w-3 h-3 bg-blue-400/80"
         id="right"
